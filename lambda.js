468a28a0acb0ae7571f3191f0faa21c68842b4df
Hariz9 <hariz.naufal@live.iium.edu.my>
2022-08-22 23:23:13 +0800

    first commit

--- lambda.js
+++
@@ -1,6 +0,0 @@
-const awsServerlessExpress = require('aws-serverless-express');
-const app = require('./src/index');
-const server = awsServerlessExpress.createServer(app)
-
-
-module.exports.universal = (event, context) => awsServerlessExpress.proxy(server, event, context);
